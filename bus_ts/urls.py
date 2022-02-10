31456cd5a9ff36dda73e19ea76eaf9178ee1405b
Mohammed Al Ameen <ameenmohammed2311@gmail.com>
2022-02-10 13:59:09 +0100

    added more styles and stuff

--- bus_ts/urls.py
+++ bus_ts/urls.py
@@ -2,10 +2,18 @@
 bus_ts Root URL Configuration
 """
 from django.contrib import admin
+from django.shortcuts import redirect
 from django.urls import include, path
+from django.shortcuts import redirect
+
+
+def index_redirect(_):
+    return redirect("tickets:bus-route-list")
+
 
 urlpatterns = [
     path("admin/", admin.site.urls),
+    path("", index_redirect),
     path("accounts/", include("accounts.urls")),
     path("tickets/", include("tickets.urls")),
 ]
